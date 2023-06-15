083014a824723fa84c6cba3dfe56babcaacd3b8c
vagrant <vagrant@vagrant.vm>
2023-06-15 13:30:28 +0000

    Fix CMakelists

--- vision/src/main.cpp
+++ vision/src/main.cpp
@@ -2,9 +2,9 @@
 This is the main file of the image processing system.
 Make any non-algorithm changes here.
 */
-#include "lodepng.hpp"
-#include "yuv422ToRGB.hpp"
-#include "RGBToPNG.hpp"
+#include "lib/lodepng.hpp"
+#include "src/yuv422ToRGB.hpp"
+#include "src/RGBToPNG.hpp"
 #include <malloc.h>
 #include <limits.h>
 #include <stdio.h>
@@ -23,35 +23,35 @@ Make any non-algorithm changes here.
 // #include "utils/speech.hpp"
 #include "utils/Timer.hpp"
 // #include "blackboard/Blackboard.hpp"
-
+ 
 using namespace std;
-
+ 
 #define IMAGE_WIDTH 640
 #define IMAGE_HEIGHT 480
-
+ 
 int main(int argc, char* argv[]){
-
+ 
     NaoCamera *cam = new NaoCamera("/dev/video-bottom", IO_METHOD_MMAP, kVGA);
-
+ 
     const uint8_t *buffer = cam->get(V4L2_PIX_FMT_YUYV);
     int bufferSize = IMAGE_WIDTH * IMAGE_HEIGHT * 3;
     cout << "Tamanho do buffer: " << bufferSize << " bytes" << endl; //imprimindo tamanho do buffer
-
+ 
     for (int j = 0; j < bufferSize; j++) {
         cout << "Valor do buffer[" << j << "]: " << static_cast<int>(buffer[j]) << endl;
     }
-
+ 
     // Converting from YUV422 to RGB32
     uint8_t RGBBuffer[bufferSize * 4];
     yuv422_to_rgb32_int(buffer, RGBBuffer, IMAGE_WIDTH, IMAGE_HEIGHT);
-
+ 
     // Expanding to RGBA
     convertoToRGBA(RGBBuffer);
-
+ 
     // Generating PNG Image
     generatePNG(generateImageName(), RGBBuffer, IMAGE_WIDTH, IMAGE_HEIGHT);
-
+ 
     delete cam;
-
+ 
     return 0;
 }
