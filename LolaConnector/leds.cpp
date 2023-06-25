65bef737b0974163f517c97a1d1b4c562e1c923b
DanielsAlmeida <daniel.keim@estudante.ufjf.br>
2023-06-25 18:44:20 -0300

    New colors

--- LolaConnector/leds.cpp
+++ LolaConnector/leds.cpp
@@ -3,3 +3,6 @@
 const RGB RGB::RED{1.f, 0.f, 0.f};
 const RGB RGB::GREEN{0.f, 1.f, 0.f};
 const RGB RGB::BLUE{0.f, 0.f, 1.f};
+const RGB RGB::OFF{0.f, 0.f, 0.f};
+const RGB RGB::PURPLE{0.5f, 0.f, 0.5f};
+const RGB RGB::ORANGE{1.f, 0.5f, 0.f};
