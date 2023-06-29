14d035de5ce2ed491f4a90d5319eacc8080db1d5
Lucas Chaves <lucasvchaves@gmail.com>
2023-06-29 16:10:00 -0300

    changes to gitignore

--- vision/build/cmake_install.cmake
+++ vision/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/y3t/RinoRobocup/SPL-Robogel/vision
+# Install script for directory: /home/y3t/RoboGel/SPL-Robogel/vision
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,11 +37,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/y3t/RinoRobocup/SPL-Robogel/softwares/ctc-linux64-atom-2.8.5.10/yocto-sdk/sysroots/x86_64-naoqisdk-linux/usr/bin/i686-sbr-linux/i686-sbr-linux-objdump")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -50,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/y3t/RinoRobocup/SPL-Robogel/vision/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/y3t/RoboGel/SPL-Robogel/vision/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
