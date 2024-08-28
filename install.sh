acebcc2f8a14db613a0ebcf6612aca73129afcca
Lucas <lucasvchaves@gmail.com>
2024-08-28 18:49:15 -0300

    Refactors install script using our asset repos and making unzip quieter

--- install.sh
+++ install.sh
@@ -8,15 +8,20 @@ ROBOGEL_CHECKOUT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 CTC_FILENAME="ctc-linux64-atom-2.8.5.10" #maybe have the option to pass the ctc as argument --cross-toolchain=~/Downloads/ctc-linux64-atom-2.8.5.10
 # eigen-3.4.0.zip
 cd "${ROBOGEL_CHECKOUT_DIR}/softwares"
-# https://github.com/rinobot-team/SPL-Robogel-Assets/releases/download/v0.1/ctc-linux64-atom-2.8.5.10.zip
-unzip ${CTC_FILENAME}.zip
+CTC_URL="https://github.com/rinobot-team/SPL-Robogel-Assets/releases/download/v0.1/ctc-linux64-atom-2.8.5.10.zip"
+echo "Downloading CTCToolchain..."
+wget ${CTC_URL}
+unzip -q ${CTC_FILENAME}.zip
+rm -f ${CTC_FILENAME}.zip
 
 # back to base dir
 cd "${ROBOGEL_CHECKOUT_DIR}"
+echo "Relocating QI Toolchain..."
 ${ROBOGEL_CHECKOUT_DIR}/softwares/${CTC_FILENAME}/yocto-sdk/relocate_qitoolchain.sh # acho que seta a toolchain pra esse lugar aqui. Não sei ao certo
 
 # Eigen para a motion 
 EIGEN_ZIP_PATH="${ROBOGEL_CHECKOUT_DIR}/HTWKMotion/eigen/eigen.zip"
-wget -O ${EIGEN_ZIP_PATH} "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip"
-unzip ${EIGEN_ZIP_PATH} -d ${ROBOGEL_CHECKOUT_DIR}/HTWKMotion/eigen/
+echo "Downloading Eigen..."
+wget -O ${EIGEN_ZIP_PATH} "https://github.com/rinobot-team/SPL-Robogel-Assets/releases/download/v0.1/eigen-3.4.0.zip"
+unzip -q ${EIGEN_ZIP_PATH} -d ${ROBOGEL_CHECKOUT_DIR}/HTWKMotion/eigen/
 
